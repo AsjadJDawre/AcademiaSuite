ec4f1e02cbca1f38d583bbe3b57a03284d698ec7
Asjad Johar Dawre <dawreasjad72@gmail.com>
2024-07-27 21:27:00 +0530

    subject_master

--- Backend/db/statement.sql
+++ Backend/db/statement.sql
@@ -1,13 +1,16 @@
 
 -- -- Create table for subject_master
--- CREATE TABLE subject_master (
+-- CREATE TABLE new_subject_master (
 --     id INTEGER PRIMARY KEY AUTOINCREMENT,
---     year TEXT NOT NULL,
---     pattern TEXT NOT NULL,
---     semester TEXT NOT NULL,
---     subject TEXT NOT NULL,
---     branch TEXT NOT NULL,
---     course_credit INTEGER NOT NULL,
+--     year TEXT ,
+--     pattern TEXT ,
+--     semester TEXT ,
+--     subject TEXT ,
+--     subject_code TEXT,
+--     branch TEXT ,
+--     H1 ,
+--     H2 , 
+--     course_credit INTEGER ,
 --     ese_marks INTEGER,
 --     ese_passing_marks INTEGER,
 --     ese_resolution TEXT,
@@ -50,14 +53,93 @@
 --  'At least 50% of the total marks', 'Minimum 50% of the total credits must be obtained', 3);
 
 
-SELECT * FROM subject_master
+-- SELECT * FROM subject_master
 
-CREATE TABLE user (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL,
-    password TEXT NOT NULL
-)
+-- CREATE TABLE user (
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     username TEXT NOT NULL,
+--     password TEXT NOT NULL
+-- -- )
 
-INSERT INTO user (username, password) VALUES ("admin", "12345")
 
-UPDATE user SET password = "$2b$10$N2gD2H1MdUbP53AIOm8mYu1zm.fuUg0V5ca5q0OjhthbYHJY.4hb2" WHERE id = 1;
+-- Update your Table Schema
+
+-- CREATE TABLE newsubject_master (
+--   id INTEGER PRIMARY KEY,
+--   year TEXT,
+--   pattern TEXT,
+--   branch TEXT,
+--   semester TEXT,
+--   H1 BOOLEAN,
+--   H2 BOOLEAN,
+--   subject_name TEXT,
+--   subject_code TEXT,
+--   no_of_credit INTEGER,
+--   h1_credit INTEGER,
+--   h2_credit INTEGER,
+--   ese_oom INTEGER,
+--   ese_pm INTEGER,
+--   ese_res INTEGER,
+--   pr_oom INTEGER,
+--   pr_pm INTEGER,
+--   pr_res INTEGER,
+--   or_oom INTEGER,
+--   or_pm INTEGER,
+--   or_res INTEGER,
+--   ia_oom INTEGER,
+--   ia_pm INTEGER,
+--   ia_res INTEGER,
+--   tw_oom INTEGER,
+--   tw_pm INTEGER,
+--   tw_res INTEGER,
+--   overall_passing_mark INTEGER
+--   overall_passing_mark2 INTEGER
+-- );
+
+
+-- Some Dummy Data 
+-- INSERT INTO newsubject_master (
+--   id, year, pattern, branch, semester, H1, H2, 
+--   subject_name, subject_code, no_of_credit, h1_credit, h2_credit, 
+--   ese_oom, ese_pm, ese_res, pr_oom, pr_pm, pr_res, 
+--   or_oom, or_pm, or_res, ia_oom, ia_pm, ia_res, 
+--   tw_oom, tw_pm, tw_res, overall_passing_mark
+-- ) VALUES 
+-- (
+--   1, '2023', 'Pattern A', 'Engineering', 'Semester 1', 1, 0,
+--   'Mathematics', 'MATH101', 3, 1, 1,
+--   100, 40, 60, 50, 20, 30,
+--   30, 15, 15, 20, 10, 10,
+--   25, 15, 10, 50
+-- ),
+-- (
+--   2, '2023', 'Pattern B', 'Science', 'Semester 2', 0, 1,
+--   'Physics', 'PHYS202', 4, 2, 1,
+--   90, 36, 54, 40, 16, 24,
+--   25, 12, 13, 18, 9, 9,
+--   20, 10, 10, 45
+-- ),
+-- (
+--   3, '2024', 'Pattern C', 'Arts', 'Semester 1', 1, 1,
+--   'History', 'HIST303', 2, 1, 1,
+--   80, 32, 48, 35, 14, 21,
+--   20, 10, 10, 15, 7, 8,
+--   30, 15, 15, 40
+-- );
+
+-- DROP Old table
+-- DROP TABLE IF EXISTS subject_master;
+
+-- RENAME the new table 
+-- ALTER TABLE newsubject_master RENAME TO subject_master;
+
+
+
+ALTER TABLE subject_master ADD COLUMN overall_passing_mark2 INTEGER;
+
+
+-- ALTER TABLE subject_master ADD COLUMN subject_code TEXT;
+
+-- INSERT INTO user (username, password) VALUES ("admin", "12345")
+
+-- UPDATE user SET password = "$2b$10$N2gD2H1MdUbP53AIOm8mYu1zm.fuUg0V5ca5q0OjhthbYHJY.4hb2" WHERE id = 1;
